e01bddb91b5af71633f71e4922fe3992d612c73c
neha <nehapunnath@gmail.com>
2025-05-27 11:32:19 +0530

    'sixth'

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import ContextApi from './context/ContextApi.jsx'
+// import ContextApi from './context/ContextApi.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-    <ContextApi>
+    {/* <ContextApi> */}
     <App />
-    </ContextApi>
+    {/* </ContextApi> */}
     </BrowserRouter>
   </StrictMode>,
 )
